47c8b8065c5e775ac43a9b3f8a7e5f0a2865fee2
caotuan <caotuan1994hk@gmail.com>
2022-08-23 16:30:02 +0700

    update

--- ss6_jpa/exercise/list_blog_new/src/main/java/com/example/repository/IBlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBlogRepository extends JpaRepository<Blog, Integer> {
-}
