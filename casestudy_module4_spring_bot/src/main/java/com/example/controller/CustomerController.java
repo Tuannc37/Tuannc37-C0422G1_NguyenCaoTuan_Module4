de6bb092ac944008715f73c4f16c488408cf20b8
caotuan <caotuan1994hk@gmail.com>
2022-08-28 21:24:11 +0700

    update

--- casestudy_module4_spring_bot/src/main/java/com/example/controller/CustomerController.java
+++ casestudy_module4_spring_bot/src/main/java/com/example/controller/CustomerController.java
@@ -1,17 +1,21 @@
 package com.example.controller;
 
+import com.example.dto.CustomerDto;
 import com.example.model.customer.Customer;
 import com.example.service.customer.ICustomerService;
 import com.example.service.customer.ICustomerTypeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -34,13 +38,21 @@ public class CustomerController {
 
     @GetMapping("showCreateCustomer")
     public String showCreateCustomer(Model model){
-        model.addAttribute("customer",new Customer());
+        model.addAttribute("customerDto",new CustomerDto());
         model.addAttribute("customerTypeList",iCustomerTypeService.findAll());
         return "customer/customer_create";
     }
 
     @PostMapping("create")
-    public String create(@ModelAttribute Customer customer, RedirectAttributes redirectAttributes){
+    public String create(@ModelAttribute @Valid CustomerDto customerDto, BindingResult
+            bindingResult, RedirectAttributes redirectAttributes,Model model){
+        new CustomerDto().validate(customerDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("customerTypeList",iCustomerTypeService.findAll());
+            return "customer/customer_create";
+        }
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDto,customer);
         iCustomerService.create(customer);
         redirectAttributes.addFlashAttribute("mess","Thêm mơi thành công!");
         return "redirect:/customer";
@@ -49,14 +61,22 @@ public class CustomerController {
     @GetMapping("showEditCustomer/{id}")
     public String showEditCustomer(@PathVariable int id, Model model) {
         model.addAttribute("customerTypeList",iCustomerTypeService.findAll());
-        model.addAttribute("customer", iCustomerService.findById(id));
+        model.addAttribute("customerDto", iCustomerService.findById(id));
         return "customer/customer_edit";
     }
 
     @PostMapping("edit/{id}")
-    public String edit(@ModelAttribute("customer") Customer customer,RedirectAttributes redirectAttributes) {
+    public String edit(@ModelAttribute @Valid CustomerDto customerDto, BindingResult
+            bindingResult, RedirectAttributes redirectAttributes,Model model) {
+        new CustomerDto().validate(customerDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("customerTypeList",iCustomerTypeService.findAll());
+            return "customer/customer_edit";
+        }
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDto,customer);
         iCustomerService.update(customer);
-        redirectAttributes.addFlashAttribute("mess", "Sửa thành công!");
+        redirectAttributes.addFlashAttribute("mess","Cập nhật thành công!");
         return "redirect:/customer";
     }
 
--- casestudy_module4_spring_bot/src/main/java/com/example/controller/EmployeeController.java
+++ casestudy_module4_spring_bot/src/main/java/com/example/controller/EmployeeController.java
@@ -5,18 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
+
     @GetMapping("/employee")
     public String testEmployee(){
         return "employee/employee_list";
-    }
-
-    @GetMapping("/showEditEmployee")
-    public String showEditEmployee(){
-        return "employee/employee_edit";
-    }
-
-    @GetMapping("/showCreateEmployee")
-    public String showCreateEmployee(){
-        return "employee/employee_create";
     }
 }
