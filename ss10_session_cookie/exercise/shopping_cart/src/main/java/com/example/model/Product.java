26caba6472c6c547280104f6a8e9f7e1eb6d5a95
caotuan <caotuan1994hk@gmail.com>
2022-08-29 20:08:28 +0700

    update

---
+++ ss10_session_cookie/exercise/shopping_cart/src/main/java/com/example/model/Product.java
@@ -0,0 +1,76 @@
+package com.example.model;
+
+import javax.persistence.*;
+
+@Entity(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String code;
+    private String name;
+    private Integer quantity;
+    private Double price;
+    private String url;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String code, String name, Integer quantity, Double price, String url) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
+
---
+++ ss10_session_cookie/practice/hit_count/src/main/java/com/example/HitCountApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitCountApplication.class, args);
+    }
+
+}
