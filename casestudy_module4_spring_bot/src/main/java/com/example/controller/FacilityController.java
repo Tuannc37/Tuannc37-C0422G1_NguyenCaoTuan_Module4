490da5b0e19b098936a878159d655dccff911600
caotuan <caotuan1994hk@gmail.com>
2022-09-12 20:16:13 +0700

    update

--- casestudy_module4_spring_bot/src/main/java/com/example/controller/FacilityController.java
+++ casestudy_module4_spring_bot/src/main/java/com/example/controller/FacilityController.java
@@ -1,18 +1,25 @@
 package com.example.controller;
 
+import com.example.dto.CustomerDto;
+import com.example.dto.FacilityDto;
+import com.example.model.customer.Customer;
 import com.example.model.facility.Facility;
 import com.example.service.facility.IFacilityTypeService;
 import com.example.service.facility.IFacilityService;
 import com.example.service.facility.IRenTypeService;
 
+import com.sun.javafx.binding.BidirectionalBinding;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -38,29 +45,47 @@ public class FacilityController {
 
     @GetMapping("showCreateFacility")
     public String showCreate(Model model) {
-        model.addAttribute("facility",new Facility());
+        model.addAttribute("facilityDto",new FacilityDto());
         model.addAttribute("facilityTypeList",iFacilityTypeService.findAll());
         model.addAttribute("rentTypeList",iRenTypeService.findAll());
         return "facility/facility_create";
     }
 
     @PostMapping("createFacility")
-    public String create(@ModelAttribute Facility facility, RedirectAttributes redirectAttributes) {
+    public String create(@ModelAttribute @Valid FacilityDto facilityDto, BindingResult
+            bindingResult, RedirectAttributes redirectAttributes,Model model) {
+        new FacilityDto().validate(facilityDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("facilityTypeList",iFacilityTypeService.findAll());
+            model.addAttribute("rentTypeList",iRenTypeService.findAll());
+            return "facility/facility_create";
+        }
+        Facility facility = new Facility();
+        BeanUtils.copyProperties(facilityDto,facility);
         iFacilytiService.create(facility);
-        redirectAttributes.addFlashAttribute("mess", "Thêm mới thành công!!");
+        redirectAttributes.addFlashAttribute("mess","Thêm mơi thành công!");
         return "redirect:/facility";
     }
 
     @GetMapping("/showEditFacility/{id}")
     public String showEdit(@PathVariable int id, Model model) {
-        model.addAttribute("facility", iFacilytiService.findByFacilityId(id));
+        model.addAttribute("facilityDto", iFacilytiService.findByFacilityId(id));
         model.addAttribute("facilityTypeList",iFacilityTypeService.findAll());
         model.addAttribute("rentTypeList",iRenTypeService.findAll());
         return "facility/facility_edit";
     }
 
     @PostMapping("/editFacility")
-    public String edit(@ModelAttribute Facility facility, RedirectAttributes redirectAttributes) {
+    public String edit(@ModelAttribute @Valid FacilityDto facilityDto, BindingResult
+            bindingResult, RedirectAttributes redirectAttributes,Model model) {
+        new FacilityDto().validate(facilityDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("facilityTypeList",iFacilityTypeService.findAll());
+            model.addAttribute("rentTypeList",iRenTypeService.findAll());
+            return "facility/facility_edit";
+        }
+        Facility facility = new Facility();
+        BeanUtils.copyProperties(facilityDto,facility);
         iFacilytiService.update(facility);
         redirectAttributes.addFlashAttribute("mess", "Cập nhật thành công");
         return "redirect:/facility";
---
+++ casestudy_module4_spring_bot/src/main/java/com/example/dto/FacilityDto.java
@@ -0,0 +1,153 @@
+package com.example.dto;
+
+import com.example.model.facility.FacilityType;
+import com.example.model.facility.RentType;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+public class FacilityDto implements Validator {
+    private Integer id;
+
+    @NotBlank(message = "không được để trống")
+    @Pattern(regexp = "^([A-Z0-9])[a-z0-9]+((\\s[A-Z0-9])[a-z0-9]+)+$", message = "2.Tên dịch vụ được phép chứa số.Và các kí tự đầu tiên của mỗi từ phải viết hoa")
+    private String name;
+
+    @NotNull(message = "không được để trống")
+    @Min(value = 0, message = "Diện tích phải là số dương")
+    private Integer area;
+
+    @NotNull(message = "không được để trống")
+    @Min(value = 0, message = "Giá phải là số dương")
+    private Double cost;
+
+    @NotNull(message = "không được để trống")
+    @Min(value = 0, message = "Số người phải là số dương")
+    private Integer maxPeople;
+
+    @NotNull(message = "Không được để trống")
+    private String standardRoom;
+    private String descriptionOtherConvenience;
+
+    @Min(value = 0, message = "Diện tích phải là số dương")
+    private Double poolArea;
+
+    @Min(value = 0, message = "Số tầng phải là số dương")
+    private Integer numberOfFloors;
+    private String facilityFree;
+    private RentType rentType;
+
+    private FacilityType facilityType;
+
+    public FacilityDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Integer getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(Integer maxPeople) {
+        this.maxPeople = maxPeople;
+    }
+
+    public String getStandardRoom() {
+        return standardRoom;
+    }
+
+    public void setStandardRoom(String standardRoom) {
+        this.standardRoom = standardRoom;
+    }
+
+    public String getDescriptionOtherConvenience() {
+        return descriptionOtherConvenience;
+    }
+
+    public void setDescriptionOtherConvenience(String descriptionOtherConvenience) {
+        this.descriptionOtherConvenience = descriptionOtherConvenience;
+    }
+
+    public Double getPoolArea() {
+        return poolArea;
+    }
+
+    public void setPoolArea(Double poolArea) {
+        this.poolArea = poolArea;
+    }
+
+    public Integer getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(Integer numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    public String getFacilityFree() {
+        return facilityFree;
+    }
+
+    public void setFacilityFree(String facilityFree) {
+        this.facilityFree = facilityFree;
+    }
+
+    public RentType getRentType() {
+        return rentType;
+    }
+
+    public void setRentType(RentType rentType) {
+        this.rentType = rentType;
+    }
+
+    public FacilityType getFacilityType() {
+        return facilityType;
+    }
+
+    public void setFacilityType(FacilityType facilityType) {
+        this.facilityType = facilityType;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+    }
+}
